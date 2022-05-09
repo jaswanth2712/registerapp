f7353951107a11c2358b20999e8bf9d3ad6354f3
jaswanth2712 <104197700+jaswanth2712@users.noreply.github.com>
2022-05-09 18:08:30 +0530

    Add project files.

---
+++ Controllers/BuildController.cs
@@ -0,0 +1,68 @@
+﻿using registerapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace registerapp.Controllers
+{
+    public class BuildController : Controller
+    {
+        logindbEntities db = new logindbEntities();
+        // GET: Build
+        public ActionResult Index()
+        {
+            return View(db.logintables.ToList());
+        }
+        public ActionResult signin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult signin(logintable lt)
+        {
+            if(db.logintables.Any(x => x.Mail == lt.Mail))
+            {
+                ViewBag.Notification = "This account has already existed";
+                return View();
+            }
+            else
+            {
+                db.logintables.Add(lt);
+                db.SaveChanges();
+
+                Session["Id"] = lt.ID.ToString();
+                Session["Username"] = lt.Mail.ToString();
+                return RedirectToAction("Index", "Build");
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult login(logintable lt)
+        {
+            var checkLogin = db.logintables.Where(x => x.Mail.Equals(lt.Mail) && x.Password.Equals(lt.Password)).FirstOrDefault();
+            if (checkLogin != null)
+            {
+
+                Session["Id"] = lt.ID.ToString();
+                Session["Username"] = lt.Mail.ToString();
+                return RedirectToAction("create", "dinner");
+            }
+            else
+            {
+                ViewBag.Notification = "wrong username or password";
+            }
+            return View();
+        }
+    }
+}
---
+++ Controllers/dinnerController.cs
@@ -0,0 +1,92 @@
+﻿using registerapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace registerapp.Controllers
+{
+    public class dinnerController : Controller
+    {
+        dinnerrep dp = new dinnerrep();
+        // GET: dinner
+        public ActionResult create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult create(Dinner d)
+        {
+
+            Dinner dd = new Dinner();
+
+            dd.DinnerID = d.DinnerID;
+            dd.Title = d.Title;
+            dd.EventDate = d.EventDate;
+            dd.Description = d.Description;
+            dd.HostedBy = d.HostedBy;
+            dd.ContactPhone = d.ContactPhone;
+            dd.Address = d.Address;
+            dd.Country = d.Country;
+            dd.Latitude = d.Latitude;
+            dd.Longitude = d.Longitude;
+            dp.Add(dd);
+
+            // Persist Changes
+            dp.Save();
+            return View();
+        }
+        public ActionResult list()
+        {
+
+            return View(dp.FindAllDinners());
+
+        }
+        public ActionResult delete(int id)
+        {
+            dinnerrep dinnerRepository = new dinnerrep();
+
+            // Retrieve specific dinner by its DinnerID
+            Dinner dinner = dinnerRepository.GetDinner(id);
+
+
+            // Mark dinner to be deleted
+            dinnerRepository.Delete(dinner);
+
+            // Persist changes
+            dinnerRepository.Save();
+            return RedirectToAction("list");
+        }
+        public ActionResult update(int id)
+        {
+            Dinner data = dp.GetDinner(id);
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult update(Dinner d)
+        {
+            Dinner data = dp.GetDinner(d.DinnerID);
+
+            data.Title = d.Title;
+            data.Address = d.Address;
+            data.ContactPhone = d.ContactPhone;
+            data.Country = d.Country;
+            data.Description = d.Description;
+            data.EventDate = d.EventDate;
+            data.HostedBy = d.HostedBy;
+            data.Latitude = d.Latitude;
+            data.Longitude = d.Longitude;
+            dp.Save();
+            return View();
+        }
+        public ActionResult viewdetails(int id)
+        {
+            Dinner data = dp.GetDinner(id);
+            return View(data);
+
+        }
+
+
+    }
+}
